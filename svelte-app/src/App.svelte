d651e749aef3d0d7dac0d499a324dea4709d9656
xaoccc <xaocccc@gmail.com>
2025-10-24 15:17:38 +0300

    update

--- svelte-app/src/App.svelte
+++ svelte-app/src/App.svelte
@@ -8,8 +8,8 @@
     let error = $state(false);
     let units = $state(["c", "kmh", "mm"]);
     let dropdownUnitsShow = $state(false);
-    const currentWeekDay = new Date
-    let weekdayShow = $state((currentWeekDay.getDay()).toString());
+    const currentWeekDay = new Date();
+    let weekdayShow = $state(currentWeekDay.getDay().toString());
     let hourlyData = $state([]);
     let dailyData = $state([]);
 
@@ -18,7 +18,6 @@
         hourlyData = [];
 
         weather.hourly.time.forEach((value, i) => {
-            
             if (value.getDay() == weekdayShow) {
                 hourlyData.push([
                     value.getHours(),
@@ -104,11 +103,16 @@
                 return "icon-sunny.webp";
             case [1, 2].includes(code):
                 return "icon-partly-cloudy.webp";
-            case [3,4,5,6,7,8,9].includes(code):
+            case [3, 4, 5, 6, 7, 8, 9].includes(code):
                 return "icon-overcast.webp";
-            case [10, 11, 12, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49].includes(code):
+            case [10, 11, 12, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49].includes(
+                code,
+            ):
                 return "icon-fog.webp";
-            case [21, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 78, 80, 81, 82].includes(code):
+            case [
+                21, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74,
+                75, 76, 77, 78, 78, 80, 81, 82,
+            ].includes(code):
                 return "icon-rain.webp";
             case [90, 91, 99].includes(code):
                 return "icon-storm.webp";
@@ -181,137 +185,153 @@
 
 {#if error}
     <ApiError />
-
 {:else}
-<main>
-    <h1>How's the sky looking today?</h1>
-    <div class="search-bar">
-        <label for="seach-bar"></label>
-        <input
-            type="text"
-            id="seach-bar"
-            placeholder="Search for a place..."
-            bind:value={city}
-        />
-        <button onclick={handleSearch}>Search</button>
-    </div>
-    <section class="container">
-        <section class="left-container">
-            <div class="current">
-                <div class="current-header">
-                    <div class="current-location">
-                        <p class="location">
-                            {weather
-                                ? `${weather.name}, ${weather.country}`
-                                : "Berlin, Germany"}
-                        </p>
-                        <p class="date">
-                            {new Date().toLocaleDateString([], {
-                                weekday: "long",
-                                month: "short",
-                                day: "numeric",
-                                year: "numeric",
-                            })}
-                        </p>
-                    </div>
-                    <div class="temperature">
-                        <img
-                            src={weather
-                                ? weatherCond(weather.current.weatherCode)
-                                : "icon-sunny.webp"}
-                            alt="icon-weather"
-                        />
-                        <p>{`${showTemperature(weather, units, "normal")}º`}</p>
-                    </div>
-                </div>
-                <div class="current-more-info">
-                    <div>
-                        <p>Feels Like</p>
-                        <p>
-                            {`${showTemperature(weather, units, "feelsLike")}º`}
-                        </p>
-                    </div>
-                    <div>
-                        <p>Humidity</p>
-                        <p>{`${weather ? weather.current.humidity : 46}%`}</p>
-                    </div>
-                    <div>
-                        <p>Wind</p>
-                        <p>
-                            {`${showWind(weather, units)} ${units[1] == "kmh" ? " km/h" : " mph"}`}
-                        </p>
-                    </div>
-                    <div>
-                        <p>Precipitation</p>
-                        <p>
-                            {`${showPrecipitation(weather, units)} ${units[2] == "mm" ? " mm" : " in"}`}
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="daily">
-                {#if dailyData}
-                    {#each dailyData as data}
-                        <div>
-                            <p>{data[0]}</p>
-                            <img
-                                src={weatherCond(data[1])}
-                                class="weather-icon"
-                                alt="weather-icon"
-                            />
-                            <div class="daily-temp">
-                                <p>{(units[0] == "c") ? data[2].toFixed(0) : toFarenheit(data[2]).toFixed(0)}º</p>
-                                <p>{(units[0] == "c") ? data[3].toFixed(0) : toFarenheit(data[3]).toFixed(0)}º</p>
-                            </div>
+    <main>
+        <h1>How's the sky looking today?</h1>
+        <div class="search-bar">
+            <label for="seach-bar"></label>
+            <input
+                type="text"
+                id="seach-bar"
+                placeholder="Search for a place..."
+                bind:value={city}
+            />
+            <button onclick={handleSearch}>Search</button>
+        </div>
+        <section class="container">
+            <section class="left-container">
+                <div class="current">
+                    <div class="current-header">
+                        <div class="current-location">
+                            <p class="location">
+                                {weather
+                                    ? `${weather.name}, ${weather.country}`
+                                    : "Berlin, Germany"}
+                            </p>
+                            <p class="date">
+                                {new Date().toLocaleDateString([], {
+                                    weekday: "long",
+                                    month: "short",
+                                    day: "numeric",
+                                    year: "numeric",
+                                })}
+                            </p>
                         </div>
-                    {/each}
-                {/if}
-            </div>
-        </section>
-        <section class="hourly">
-            <div class="hourly-header">
-                <p>Hourly forecast</p>
-                <select
-                    onchange={(e) => {
-                        const select = e.target as HTMLSelectElement;
-                        weekdayShow = select.value;
-                        logDay();
-                    }}
-                    name="dropdown-weekday"
-                    bind:value={weekdayShow}
-                    id="dropdown-weekday"
-                    class="dropdown-weekday"
-                >
-                    <option value="1">Monday</option>
-                    <option value="2">Tuesday</option>
-                    <option value="3">Wednesday</option>
-                    <option value="4">Thursday</option>
-                    <option value="5">Friday</option>
-                    <option value="6">Saturday</option>
-                    <option value="0">Sunday</option>
-                </select>
-            </div>
-            <div class="hourly-data">
-                {#if hourlyData.length > 0}
-                    {#each hourlyData as data}
+                        <div class="temperature">
+                            <img
+                                src={weather
+                                    ? weatherCond(weather.current.weatherCode)
+                                    : "icon-sunny.webp"}
+                                alt="icon-weather"
+                            />
+                            <p>
+                                {`${showTemperature(weather, units, "normal")}º`}
+                            </p>
+                        </div>
+                    </div>
+                    <div class="current-more-info">
                         <div>
+                            <p>Feels Like</p>
+                            <p>
+                                {`${showTemperature(weather, units, "feelsLike")}º`}
+                            </p>
+                        </div>
+                        <div>
+                            <p>Humidity</p>
+                            <p>
+                                {`${weather ? weather.current.humidity : 46}%`}
+                            </p>
+                        </div>
+                        <div>
+                            <p>Wind</p>
+                            <p>
+                                {`${showWind(weather, units)} ${units[1] == "kmh" ? " km/h" : " mph"}`}
+                            </p>
+                        </div>
+                        <div>
+                            <p>Precipitation</p>
+                            <p>
+                                {`${showPrecipitation(weather, units)} ${units[2] == "mm" ? " mm" : " in"}`}
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="daily">
+                    {#if dailyData}
+                        {#each dailyData as data}
                             <div>
+                                <p>{data[0]}</p>
                                 <img
-                                    src={weatherCond(data[2])}
+                                    src={weatherCond(data[1])}
                                     class="weather-icon"
                                     alt="weather-icon"
                                 />
-                                <div>
-                                    {data[0] > 12 ? data[0] - 12 : data[0]}
-                                    {data[0] > 12 ? "PM" : "AM"}
+                                <div class="daily-temp">
+                                    <p>
+                                        {units[0] == "c"
+                                            ? data[2].toFixed(0)
+                                            : toFarenheit(data[2]).toFixed(0)}º
+                                    </p>
+                                    <p>
+                                        {units[0] == "c"
+                                            ? data[3].toFixed(0)
+                                            : toFarenheit(data[3]).toFixed(0)}º
+                                    </p>
                                 </div>
                             </div>
-                            <div>{(units[0] == "c") ? data[1].toFixed(0) : toFarenheit(data[1]).toFixed(0)}º</div>
-                        </div>
-                    {/each}
-                {/if}
-            </div>
+                        {/each}
+                    {/if}
+                </div>
+            </section>
+            <section class="hourly">
+                <div class="hourly-header">
+                    <p>Hourly forecast</p>
+                    <!-- TO DO: make the dropdown customizable, using div + ul + li -->
+                    <select
+                        onchange={(e) => {
+                            const select = e.target as HTMLSelectElement;
+                            weekdayShow = select.value;
+                            logDay();
+                        }}
+                        name="dropdown-weekday"
+                        bind:value={weekdayShow}
+                        id="dropdown-weekday"
+                        class="dropdown-weekday"
+                    >
+                        <option value="1">Monday</option>
+                        <option value="2">Tuesday</option>
+                        <option value="3">Wednesday</option>
+                        <option value="4">Thursday</option>
+                        <option value="5">Friday</option>
+                        <option value="6">Saturday</option>
+                        <option value="0">Sunday</option>
+                    </select>
+                </div>
+                <div class="hourly-data">
+                    {#if hourlyData.length > 0}
+                        {#each hourlyData as data}
+                            <div>
+                                <div>
+                                    <img
+                                        src={weatherCond(data[2])}
+                                        class="weather-icon"
+                                        alt="weather-icon"
+                                    />
+                                    <div>
+                                        {data[0] > 12 ? data[0] - 12 : data[0]}
+                                        {data[0] > 12 ? "PM" : "AM"}
+                                    </div>
+                                </div>
+                                <div>
+                                    {units[0] == "c"
+                                        ? data[1].toFixed(0)
+                                        : toFarenheit(data[1]).toFixed(0)}º
+                                </div>
+                            </div>
+                        {/each}
+                    {/if}
+                </div>
+            </section>
         </section>
-    </section>
-</main>
+    </main>
 {/if}
